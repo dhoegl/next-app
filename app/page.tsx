f0c5202ceb941cd9d72ff4e84288cff34a05c655
dhoegl <firebird@hoeglund.com>
2024-02-17 20:29:46 -0800

    Second Commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main><h1>Hello World!</h1></main>
+    <main><h1>Hello World!!! Again! And Again!</h1></main>
   )
 }
